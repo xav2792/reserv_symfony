24c5f4886a372f64a76d78caba5e518a274e79a2
xav2792 <xavier.engels@laposte.net>
2016-03-01 16:46:59 +0100

    modif route

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class LoginController extends Controller
 {
