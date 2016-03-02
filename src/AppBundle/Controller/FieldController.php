97d44cf8387c0a0c929de61a3c9d93ccf03dfe46
Baptiste-Flesch <baptiste.flesch@gmail.com>
2016-03-02 11:53:25 +0100

    Field

---
+++ src/AppBundle/Controller/FieldController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Field;
+use AppBundle\Form\FieldType;
+
+/**
+ * Field controller.
+ *
+ * @Route("/field")
+ */
+class FieldController extends Controller
+{
+    /**
+     * Lists all Field entities.
+     *
+     * @Route("/", name="field_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $fields = $em->getRepository('AppBundle:Field')->findAll();
+
+        return $this->render('field/index.html.twig', array(
+            'fields' => $fields,
+        ));
+    }
+
+    /**
+     * Creates a new Field entity.
+     *
+     * @Route("/new", name="field_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $field = new Field();
+        $form = $this->createForm('AppBundle\Form\FieldType', $field);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($field);
+            $em->flush();
+
+            return $this->redirectToRoute('field_show', array('id' => $field->getId()));
+        }
+
+        return $this->render('field/new.html.twig', array(
+            'field' => $field,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Field entity.
+     *
+     * @Route("/{id}", name="field_show")
+     * @Method("GET")
+     */
+    public function showAction(Field $field)
+    {
+        $deleteForm = $this->createDeleteForm($field);
+
+        return $this->render('field/show.html.twig', array(
+            'field' => $field,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Field entity.
+     *
+     * @Route("/{id}/edit", name="field_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Field $field)
+    {
+        $deleteForm = $this->createDeleteForm($field);
+        $editForm = $this->createForm('AppBundle\Form\FieldType', $field);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($field);
+            $em->flush();
+
+            return $this->redirectToRoute('field_edit', array('id' => $field->getId()));
+        }
+
+        return $this->render('field/edit.html.twig', array(
+            'field' => $field,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Field entity.
+     *
+     * @Route("/{id}", name="field_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Field $field)
+    {
+        $form = $this->createDeleteForm($field);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($field);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('field_index');
+    }
+
+    /**
+     * Creates a form to delete a Field entity.
+     *
+     * @param Field $field The Field entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Field $field)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('field_delete', array('id' => $field->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Field.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Field
+ *
+ * @ORM\Table(name="field")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FieldRepository")
+ */
+class Field
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255)
+     */
+    private $location;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Field
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Field
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Field
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     *
+     * @return Field
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+}
+
